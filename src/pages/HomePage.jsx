159b56d70de8bed3cf5174f4661d05a4689ea2ca
Joy Brar <joybrar2001@gmail.com>
2024-05-21 15:41:48 +0530

    Testing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -65,7 +65,7 @@ const HomePage = () => {
             Welcome to
           </h2>
           <h1 className='font-semibold text-2xl sm:text-3xl md:text-5xl -mt-0.5 max-w-lg mx-auto'>
-            The first RSA Facing Pixel AI Quiz
+            The first RSA Facing Pixel AI Quiz (testing)
           </h1>
           <p className='font-medium text-sm md:text-xl text-zinc-600 mt-3 leading-tight max-w-lg mx-auto'>
             You can attempt this quiz multiple times with the RSAs in store.
